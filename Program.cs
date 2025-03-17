890d20bc363cccb9c3935ec32177fe4fe8ab2119
Poujol Raphael <poujol.raphael0@gmail.com>
2025-03-17 11:32:57 +0100

    Conversion en heures

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int nbMin = 130; // Nombre de minutes à convertir
+        int heures = nbMin / 60;
+        int minutes = nbMin % 60;
+
+        Console.WriteLine(heures + ":" + minutes); // Affiche "2:10"
+    }
+}
